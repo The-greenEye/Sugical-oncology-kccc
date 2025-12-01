1c9ab19927e4c2bd077afb1f44390657a84e427e
The-greenEye <156267582+The-greenEye@users.noreply.github.com>
2025-12-01 15:00:55 +0200

    Update phone link format in Footer component

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
             </div>
             <h3 className="text-2xl font-bold mb-4">للأستفسار او التسجيل</h3>
             <b>Sayed Salama </b>
-            <a link="tel:51147478" className="text-lg underline hover:text-pink-300 transition-colors duration-300">51147478</a>
+            <a link="tel:+96551147478" className="text-lg underline hover:text-pink-300 transition-colors duration-300">51147478</a>
             <img src="https://up6.cc/2025/10/17645854668411.jpeg" alt="Phone Icon" width={'50'} height="50" className="inline-flex ml-2" />
           </div>
           <div className="border-t border-purple-300 border-opacity-30 pt-8 mt-8">
