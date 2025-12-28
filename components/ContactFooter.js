933a5509d7a9a52238b4ed1f25599e281373453f
The-greenEye <156267582+The-greenEye@users.noreply.github.com>
2025-12-28 16:08:55 +0200

    Change phone icon div to a link for WhatsApp

--- components/ContactFooter.js
+++ components/ContactFooter.js
@@ -35,9 +35,9 @@ function ContactFooter() {
                         <div className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 bg-black pattern-grid-lg relative flex items-center justify-center group cursor-pointer">
                              {/* Mock QR Pattern */}
                              <div className="absolute inset-0 border-4 border-white border-dashed opacity-50"></div>
-                             <div className="w-10 h-10 md:w-12 md:h-12 bg-green-500 rounded-full flex items-center justify-center shadow-lg z-10 group-hover:scale-110 transition-transform">
+                             <a href="https://wa.me/+96551147478" className="w-10 h-10 md:w-12 md:h-12 bg-green-500 rounded-full flex items-center justify-center shadow-lg z-10 group-hover:scale-110 transition-transform">
                                 <div className="icon-phone text-white text-lg md:text-xl fill-current"></div>
-                             </div>
+                             </a>
                         </div>
                     </div>
                 </div>
